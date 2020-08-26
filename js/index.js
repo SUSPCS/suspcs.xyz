68e0f39b744d38097cb23b00c9449e9febe8779a
Jude Wilson <judewilson2024@gmail.com>
2020-08-26 18:26:03 -0400

    stuffs

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function noHistoryHref(gamePath) {
+    $.ajax(gamePath, {
+        success: function (response) {
+            document.getElementById("body").innerHTML = '<iframe style="width:100%;height:85vh;border:none;background-color: rgb(45, 45, 45); color:rgb(183, 183, 183);" src=' + gamePath + '></iframe>';
+        }
+    });
+}
