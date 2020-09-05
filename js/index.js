23c8ce5e08194f4b6b585c3f632c0e3e3f5cf45b
Jude Wilson <judewilson2024@gmail.com>
2020-09-05 17:46:58 -0400

    overhaul

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
-function noHistoryHref(gamePath) {
-    $.ajax(gamePath, {
-        success: function (response) {
-            document.getElementById("body").innerHTML = '<iframe style="width:100%;height:85vh;border:none;background-color: rgb(45, 45, 45); color:rgb(183, 183, 183);" src=' + gamePath + '></iframe>';
+includeHTML()
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("include");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Include not found.";
+                    }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("include");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
         }
-    });
-}
-document.addEventListener('contextmenu', event => event.preventDefault());
+    }
+}document.addEventListener('contextmenu', event => event.preventDefault());
